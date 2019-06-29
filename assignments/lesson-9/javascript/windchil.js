4daf5c268506dd1d347c33214f6dc8ebbd1b1a91
puzzleramelia <all18044@byui.edu>
2019-06-29 10:41:17 -0600

    commit js

--- assignments/lesson-9/javascript/windchil.js
+++ assignments/lesson-9/javascript/windchil.js
@@ -1,12 +1,8 @@
 
 function windchil() {
-        
-        
     var temp = parseFloat(document.getElementById("current-temp").value);
     var windspeed = parseFloat(document.getElementById("windspeed").value);
-    var ex = math.pow(windspeed, .16);
-    var windchill;
+    var ex = Math.pow(windspeed, .16);
     var windchill = 35.74 + 0.6215 * temp - 35.75 * ex + .4275 * temp * ex;
     document.getElementById("windchil").innerHTML = windchill;
-    }
-    
+}
