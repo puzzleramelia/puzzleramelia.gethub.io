911535ae59fa8649e0510a41caba16fb29ec25de
puzzleramelia <all18044@byui.edu>
2019-06-29 10:10:48 -0600

    add API to main server

---
+++ assignments/lesson-9/js/weatherapi.js
@@ -0,0 +1,10 @@
+let weatherRequest = new XMLHttpRequest();
+let apiURLstring = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=1c256240d233d9ee96f640fc2b0ed703';
+weatherRequest.open('Get', apiURLstring, true);
+weatherRequest.send();
+
+weatherRequest.onload =  function () {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+console.log(weatherData);
+document.getElementById('current-temp').innerHTML = weatherData.main.temp;
+}
