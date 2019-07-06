71df1d1e7e49d9eacbf6e543997423d121998ca6
puzzleramelia <all18044@byui.edu>
2019-07-06 10:38:47 -0600

    add to master

--- assignments/lesson-11/javascript/windchil.js
+++ assignments/lesson-11/javascript/windchil.js
@@ -1,8 +1,10 @@
 
 function windchil() {
-    var temp = parseFloat(document.getElementById("current-temp").value);
-    var windspeed = parseFloat(document.getElementById("windspeed").value);
-    var ex = Math.pow(windspeed, .16);
+    var temp = parseFloat(document.getElementById("currenttemp").value);
+    var wind = parseFloat(document.getElementById("windspeed").value);
+
+
+    var ex = Math.pow(wind, .16);
     var windchill = 35.74 + 0.6215 * temp - 35.75 * ex + .4275 * temp * ex;
     document.getElementById("windchil").innerHTML = windchill;
 }
